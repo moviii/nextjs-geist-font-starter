0bc618edc6497f53477502bba5e423b1124baa3c
Movindu Dewnaka <48215860+moviii@users.noreply.github.com>
2025-06-18 01:28:18 +0530

    Add ip-tracker-app/src/components/iptracker/MapViewer.tsx 1750190297442

---
+++ ip-tracker-app/src/components/iptracker/MapViewer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix Leaflet marker icon issues
+delete (L.Icon.Default as any).prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
+
+interface MapViewerProps {
+  latitude: number;
+  longitude: number;
+}
+
+export default function MapViewer({ latitude, longitude }: MapViewerProps) {
+  return (
+    <div className="h-[400px] w-full rounded-lg overflow-hidden shadow-lg">
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            Location: {latitude}, {longitude}
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
