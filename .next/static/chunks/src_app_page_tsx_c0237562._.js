38ae313d6353d81e45bf85905dc6a03d726ff3c4
Movindu Dewnaka <48215860+moviii@users.noreply.github.com>
2025-06-18 01:29:15 +0530

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1750190354620

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_iptracker_MapViewer_tsx_5a7ab09b._.js",
+  "static/chunks/node_modules_a319e5ba._.js",
+  "static/chunks/src_1b558de3._.js"
+],
+    source: "dynamic"
+});
